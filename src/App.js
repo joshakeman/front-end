9198e55e550e02c8394f6e0b8081006c76557f53
Josh Akeman <joshakeman@gmail.com>
2019-07-30 20:38:55 -0400

    pulling down and rendering current room

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import axios from 'axios'
 
 import sword from './images/sword1.png'
 import shield from './images/shield1.png'
@@ -10,11 +12,76 @@ import ProgressBarExample from './components/HealthBar'
 
 function App() {
 
+  const [currentRoom, setRoom] = useState('');
+
+
   function clear(divName) {
     let boxOne = document.getElementsByClassName(divName)
     console.log(boxOne[0].classList)
     boxOne[0].classList.add('blurTranslate')
   }
+
+  const register = () => {
+    const fetchData = async () => {
+      const result = await axios.post(
+       'https://murmuring-earth-14820.herokuapp.com/api/registration/', 
+       { username: "testuser1", password1:"testpassword", password2:"testpassword" }    
+       )
+
+      console.log(result)
+      // setRoom(result.data);
+    };
+
+    fetchData();
+  }
+
+  const login = () => {
+    const fetchData = async () => {
+      const result = await axios.post(
+       'https://murmuring-earth-14820.herokuapp.com/api/login/', 
+       { username: "testuser1", password:"testpassword" }    
+       )
+
+      console.log(result)
+      // setRoom(result.data);
+      localStorage.setItem('key', result.data.key)
+    };
+
+    fetchData();
+  }
+
+  const init = () => {
+
+    const fetchData = async () => {
+      const key = localStorage.getItem('key')
+      const result = await axios({
+        url: 'https://murmuring-earth-14820.herokuapp.com/api/adv/init/',
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${key}`,
+        }   
+      })
+
+      console.log(result)
+      setRoom(result.data.title);
+    };
+
+    fetchData();
+  }
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await axios.post(
+  //      'https://murmuring-earth-14820.herokuapp.com/api/registration/', 
+  //      { username: "testuser1", password1:"testpassword", password2:"testpassword" }    
+  //      )
+
+  //     console.log(result.data)
+  //     setRoom(result.data);
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
     <>
@@ -32,12 +99,15 @@ function App() {
     <div className="body-wrapper">
       <div className="game-board">
         <div className="left-column">
+          <button onClick={register}>Register</button>
+          <button onClick={login}>Login</button>
+          <button onClick={init}>Init</button>
 
         </div>
         <div className="right-column">
 
           <div className="top-half">
-          
+          Current Room: {currentRoom} 
           </div>
           <div className="bottom-half">
             <div className="items">
